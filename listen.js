7e0dff825a23e2142ebf2f0714cbba8f8cc05b00
balazs4 <balazs.varga@sealsystems.de>
2020-03-16 16:19:27 +0100

    ts as field and gracefull shutdown

--- listen.js
+++ listen.js
@@ -3,15 +3,20 @@
 const connect = require("./connect");
 const log = require("./log");
 
+let counter = 0;
 (async () => {
   try {
     const client = await connect();
-    client.watch().on("change", c =>
+    client.watch().on("change", event => {
+      console.log(event.operationType)
+      counter++;
       log("listen", {
-        insertedId: c.documentKey._id,
-        timestamp: Date.now()
-      })
-    );
+        insertedId: event.documentKey._id,
+        timestamp: new Date().toISOString(),
+        diff: Date.now() - event.fullDocument.ts
+      });
+      console.log(counter);
+    });
     console.log("Listening...");
   } catch (err) {
     log("listen", err);
--- produce.js
+++ produce.js
@@ -5,20 +5,35 @@ const log = require("./log");
 
 const createDocument = () => {
   return {
-    name: "foobar"
+    name: "foobar",
+    ts: Date.now()
   };
 };
 
+let counter = 0;
+let stop = true;
 (async () => {
   try {
     const client = await connect();
-    while (true) {
+    while (stop) {
       const doc = createDocument();
+      counter++;
       const { insertedId } = await client.insertOne(doc);
-
-      log("producer", { insertedId, timestamp: Date.now() });
+      log("producer", { insertedId, timestamp: new Date().toISOString() });
     }
   } catch (err) {
     log("producer", err);
   }
 })();
+
+process.on("SIGINT", async () => {
+  stop = false;
+  //it should depend on config
+  const { TIMEOUT = 5000 } = process.env;
+  console.log(
+    `Graceful shutdown...in order to flush the winston buffer ...please wait ${TIMEOUT} ms`
+  );
+  console.log({ counter });
+  await new Promise(res => setTimeout(res, TIMEOUT));
+  process.exit();
+});
